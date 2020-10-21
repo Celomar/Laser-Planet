5542b4c2c605a28b2fc35ddadc47cb3e857f0324
Diego Fernandez <fernandezdiego129@gmail.com>
2020-10-21 00:36:25 -0400

    Raycast for Yellow and Blue

---
+++ Planet Laser/Assets/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Transform firepoint;
+    
+    void Start()
+    {
+        RecalculateLazer();
+
+
+    }
+    void RecalculateLazer()
+    {
+        // Ray2D ray = new Ray2D (firepoint.position, -transform.up);
+
+        RaycastHit2D hit = Physics2D.Raycast(firepoint.position, -Vector2.up);
+
+        Debug.Log(hit.transform.position);
+
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
