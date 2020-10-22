17f02f8fa4ecc7ffb6365266e02403158e8120bd
AlouMena <alou_mena20@hotmail.com>
2020-10-21 22:28:25 -0400

    LASER PA LANTE

--- Planet Laser/Assets/Laser.cs
+++ Planet Laser/Assets/Laser.cs
@@ -7,31 +7,26 @@ public class Laser : MonoBehaviour
     public Transform firepoint;
     public GameObject lazer;
     public LineRenderer lazerrenderer;
-    
+
+    private RaycastHit2D hit;
+
     void Start()
     {
         RecalculateLazer();
-
-
-    }
-    void RecalculateLazer()
-    {
-        // Ray2D ray = new Ray2D (firepoint.position, -transform.up);
-
-        RaycastHit2D hit = Physics2D.Raycast(firepoint.position, -Vector2.up);
-
-        Debug.Log(hit.transform.position);
-        // Instantiate(lazer, firepoint.position, Quaternion.identity);
-
+        lazerrenderer.enabled = true;
         lazerrenderer.SetPosition(0, firepoint.position);
         lazerrenderer.SetPosition(1, hit.point);
-        lazerrenderer.SetWidth(2, 2);
-
     }
 
-  
     void Update()
     {
-        
+        RecalculateLazer();
+    }
+
+    void RecalculateLazer()
+    {
+        hit = Physics2D.Raycast(firepoint.position, -Vector2.up);
+        Debug.Log(hit.transform.name);
     }
 }
+
