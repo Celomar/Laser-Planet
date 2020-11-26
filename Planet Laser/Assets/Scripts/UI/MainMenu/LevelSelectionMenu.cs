fc30bf4a0f8a5314a9ab44bfed4639996738cacf
Luis Chiang <lahoising78@gmail.com>
2020-11-25 22:24:30 -0400

    select level view container

---
+++ Planet Laser/Assets/Scripts/UI/MainMenu/LevelSelectionMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LevelSelectionMenu : MonoBehaviour
+{
+    public MainMenu mainMenu = null;
+    public GameObject startMenu = null;
+
+    void Awake()
+    {
+        if(!mainMenu)
+        {
+            mainMenu = transform.GetComponentInParent<MainMenu>();
+        }
+    }
+
+    public void CloseLevelSelection()
+    {
+        mainMenu.TurnAllMenusOffExcept(startMenu);
+    }
+}
