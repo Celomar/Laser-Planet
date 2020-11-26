ce493043c08b6b3467bd0104a4535022bdfd238d
Luis Chiang <lahoising78@gmail.com>
2020-11-25 20:26:35 -0400

    quit game

---
+++ Planet Laser/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject[] menus = null;
+
+    void Awake()
+    {
+        if(menus == null || menus.Length < 1) PopulateMenusListFromChildren();
+        Debug.Assert(menus.Length > 0, "There are no menus for main menu");
+        TurnAllMenusOffExcept(menus[0]);
+    }
+
+    private void PopulateMenusListFromChildren()
+    {
+        int childCount = transform.childCount;
+        menus = new GameObject[childCount];
+        for(int i = 0; i < childCount; i++)
+        {
+            menus[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void TurnAllMenusOffExcept(GameObject targetMenu)
+    {
+        foreach(GameObject menu in menus)
+        {
+            menu.SetActive(menu == targetMenu);
+        }
+    }
+}
---
+++ Planet Laser/Assets/Scripts/UI/MainMenu/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public MainMenu mainMenu = null;
+    public GameObject levelSelectMenu = null;
+
+    void Awake()
+    {
+        if(!mainMenu)
+        {
+            mainMenu = transform.GetComponentInParent<MainMenu>();
+        }
+    }
+
+    public void Play()
+    {
+        mainMenu.TurnAllMenusOffExcept(levelSelectMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
