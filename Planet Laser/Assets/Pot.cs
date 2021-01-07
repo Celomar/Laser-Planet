b727adbc8896cc4fb052b49055bd316a1e26c8af
ClassicH <h_m@hotmail.es>
2021-01-07 02:22:59 -0400

    El pot se mueve

--- Planet Laser/Assets/Pot.cs
+++ Planet Laser/Assets/Pot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Pot : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,38 @@ public class Pot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.parent != null && Input.GetButtonUp("Pick up"))
+        {
+            this.transform.SetParent(null);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        
+        GameObject pot;
+        Transform astronauta;
+        Vector3 localPosition= new Vector3(1.005f,1.005f,0);
+
+        pot = this.gameObject;
+        astronauta = other.transform; //Referencia al game object
+
+        Vector3 potPosition = pot.transform.position;
+        Vector3 astPosition=other.transform.position;
+        Vector3 direction = potPosition - astPosition;
+
+        localPosition = Vector3.Scale(localPosition,direction);
+
+        
+
+        if (Input.GetButton("Pick up"))
+        {
+            if (transform.parent == null)
+            {
+                pot.transform.SetParent(astronauta, true);
+                pot.transform.localPosition = localPosition;
+            }
+        }
         
     }
 }
